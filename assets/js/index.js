49f8844d702815f4f09cecc49536b40cd8199565
sinH15 <sinh15@users.noreply.github.com>
2016-03-27 18:19:42 +0200

    added loading behaviour

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+// Function is called when the user clicks the "Sign In" button.
+function signIn(){
+  // Request an access token (login)
+  client.getAccessToken().then(function(){
+    // Redirect the page
+    $("#loading-container").fadeOut("fast");
+    setTimeout(function() {
+        window.location = "http://localhost:8080/tryout";
+    }, 300);
+  })
+
+  // Error handling
+  .catch(function(e){
+
+  });
+
+}
+
+$( document ).ready(function() {
+    // START: Stop focus on button press
+    $(".btn").mouseup(function(){
+        $(this).blur();
+    }); // -- end: focus on button press
+
+    // START: sign-in button
+    $('#sign-in').click(function () {
+
+        // Set properties for new loading gif container
+        var height = $(window).height();
+        var move = (height/2)-110;
+        $("#wrap").css('transform', 'translateY(+' + move + 'px)');
+
+        // Fade out main container, fade in logging
+        $("#main-container").fadeOut("fast");
+        $("#loading-container").fadeIn("slow");
+
+        // Launch signIn function
+        signIn();
+
+    }); // -- end: sign-in button
+
+    // START: Liten on window resize to keep content aligned
+    $(window).resize(function () {
+        if($("#loading-container").css('display') != "none") {
+            var height = $(window).height();
+            var move = (height/2)-66;
+            $("#wrap").css('transform', 'translateY(+' + move + 'px)');
+        }
+    }); // END: Listen on window resize
+});
