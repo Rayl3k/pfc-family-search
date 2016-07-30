65e4e6e7844c18fad498739cc568ba7227d46cf9
sinH15 <sinh15@users.noreply.github.com>
2016-07-30 18:26:06 +0200

    ready to start certificate process

--- assets/js/proposalExamplesLinks.js
+++ assets/js/proposalExamplesLinks.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
     // reroute when clicked proposal box
+    /*
     $("#proposal-box-1").click(function() {
         window.location = document.location.protocol + '//' + document.location.host + '/proposals/example1';
     });
@@ -8,9 +9,11 @@ $( document ).ready(function() {
     $("#proposal-box-2").click(function() {
         window.location = document.location.protocol + '//' + document.location.host + '/proposals/names-popularity';
     });
+    */
 
 
     // reroute when clicked examplple box depending on current path
+    /*
     $("#implemented-box-1").click(function() {
         var path = location.pathname.split('?')[0].slice(1);
         var redirect = path == "proposals" ? '/proposals/search' : '/examples/search';
@@ -27,5 +30,31 @@ $( document ).ready(function() {
         var path = location.pathname.split('?')[0].slice(1);
         var redirect = path == "proposals" ? '/proposals/facts' : '/examples/facts';
         window.location = document.location.protocol + '//' + document.location.host + redirect;
+    });
+    */
+
+    $(".proposal-box").click(function() {
+        alert("Specific view for proposals still not ni place!");
+    });
+
+    $("#implemented-box-1").click(function() {
+        var path = location.pathname.split('?')[0].slice(1);
+        var redirect = path == "proposals" ? '' : '/examples/search';
+        if(redirect != '') window.location = document.location.protocol + '//' + document.location.host + redirect;
+        else alert("Specific view for proposals still not ni place!");
+    });
+
+    $("#implemented-box-2").click(function() {
+        var path = location.pathname.split('?')[0].slice(1);
+        var redirect = path == "proposals" ? '' : '/examples/surnames';
+        if(redirect != '') window.location = document.location.protocol + '//' + document.location.host + redirect;
+        else alert("Specific view for proposals still not ni place!");
+    });
+
+    $("#implemented-box-3").click(function() {
+        var path = location.pathname.split('?')[0].slice(1);
+        var redirect = path == "proposals" ? '' : '/examples/facts';
+        if(redirect != '') window.location = document.location.protocol + '//' + document.location.host + redirect;
+        else alert("Specific view for proposals still not ni place!");
     });
 });
